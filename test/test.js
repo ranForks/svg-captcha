58f11e7f3540eb8f79a9fda6e9bf0e68d85fb84b
huang.lingxiang <hlx@winbaoxian.com>
2016-11-18 17:48:41 +0800

    我的代码整理工具比较纠结

--- test/test.js
+++ test/test.js
@@ -7,53 +7,59 @@ const xmlReg = /^<svg[\s\S]+\/svg>$/;
 const textTagReg = /text/;
 
 describe('svg captcha', () => {
-	it('should generate random text', () => {
-		for (let i = 0; i < 62; i++) {
-			const text = svgCaptcha.randomText();
-			assert(/^[0-9a-zA-Z]+$/.test(text));
-		}
-	});
+    it('should generate random text', () => {
+        for (let i = 0; i < 62; i++) {
+            const text = svgCaptcha.randomText();
+            assert(/^[0-9a-zA-Z]+$/.test(text));
+        }
+    });
 
-	it('should filter unwanted chars', () => {
-		const opt = {ignoreChars: '0123456789'};
-		for (let i = 0; i < 62; i++) {
-			const text = svgCaptcha.randomText(opt);
-			assert(/^[a-zA-Z]+$/.test(text));
-		}
-	});
+    it('should filter unwanted chars', () => {
+        const opt = {
+            ignoreChars: '0123456789'
+        };
+        for (let i = 0; i < 62; i++) {
+            const text = svgCaptcha.randomText(opt);
+            assert(/^[a-zA-Z]+$/.test(text));
+        }
+    });
 
-	it('should generate svg', () => {
-		assert(xmlReg.test(svgCaptcha()));
-		assert(xmlReg.test(svgCaptcha({text: 'abcd'})));
-	});
+    it('should generate svg', () => {
+        assert(xmlReg.test(svgCaptcha()));
+        assert(xmlReg.test(svgCaptcha({
+            text: 'abcd'
+        })));
+    });
 
-	it('should generate path', () => {
-		assert(!textTagReg.test(svgCaptcha({text: 'text'})));
-	});
+    it('should generate path', () => {
+        assert(!textTagReg.test(svgCaptcha({
+            text: 'text'
+        })));
+    });
 
-	it('should be fast', () => {
-		for (let i = 0; i < 100; i++) {
-			const text = svgCaptcha.randomText();
-			svgCaptcha(text);
-		}
-	}, {
-		slow: 50,
-		timeout: 100
-	});
+    it('should be fast', () => {
+        for (let i = 0; i < 100; i++) {
+            const text = svgCaptcha.randomText();
+            svgCaptcha(text);
+        }
+    }, {
+        slow: 50,
+        timeout: 100
+    });
 });
 
 const random = require('../lib/random');
 
 describe('random function', () => {
-	it('should generate random integer', () => {
-		for (let i = 0; i < 10; i++) {
-			const num = random.int(0, 10);
-			assert(num >= 0 && num <= 10);
-		}
-	});
+    it('should generate random integer', () => {
+        for (let i = 0; i < 10; i++) {
+            const num = random.int(0, 10);
+            assert(num >= 0 && num <= 10);
+        }
+    });
 
-	it('should generate grey color', () => {
-		assert(random.greyColor());
-		assert(random.greyColor(3, 4));
-	});
+    it('should generate grey color', () => {
+        assert(random.greyColor());
+        assert(random.greyColor(3, 4));
+    });
 });
