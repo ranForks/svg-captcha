24d07d0d79a576b34c1481b33ed8e0692820f3b2
huang.lingxiang <hlx@winbaoxian.com>
2016-11-18 17:48:21 +0800

    加 'use strict’;才能用

--- test/visual.js
+++ test/visual.js
@@ -1,12 +1,14 @@
+'use strict';
+
 const fs = require('fs');
 const svg = require('../');
 
 for (let i = 0; i < 10; i++) {
-	fs.writeFile(`test${i}.svg`, svg(svg.randomText()), 'utf8', err => {
-		if (err) {
-			console.error(err);
-		} else {
-			console.log('it\'s saved');
-		}
-	});
+    fs.writeFile(`test${i}.svg`, svg(svg.randomText()), 'utf8', err => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('it\'s saved');
+        }
+    });
 }
